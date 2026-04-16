23be13e3a12102205f825030157f82e4db6506a6
alisson92-tech <alissonli1992@gmail.com>
2026-04-16 23:59:29 +0000

    Nível mestre concluído

--- xadrez.c
+++ xadrez.c
@@ -1,45 +1,71 @@
 #include <stdio.h>
 
+    void ftorre(int ft){
+        if(ft > 0){
+            printf("Torre direita \n");
+            ftorre(ft - 1);
+        }
+    }
+
+   void fbispo(int fb){
+    if(fb > 0){
+
+        for(int vertical = 0; vertical < 1; vertical++){
+            for(int horizontal = 0; horizontal < 1; horizontal++){
+                printf("Bispo cima direita\n");
+            }
+        }
+
+        fbispo(fb - 1);
+    }
+}
+
+void frainha(int fr){
+        if(fr > 0){
+            printf("Rainha esquerda \n");
+            frainha(fr - 1);
+        }
+    }
+
     int main(){
       //Declaração das variaveis
        const char torre [] = "Torre";
        const char bispo [] = "Bispo";
        const char rainha[] = "Rainha";
        const char cavalo[] = "Cavalo";
-       int t = 1, b = 1 , r = 1, c = 1;
+       int t = 5, b = 5 , r = 8;
 
-       //Movimentos da Torre
-       while(t <= 5){
-        printf("%s direita. \n",torre);
-        t++;
-       }
-
-       printf("\n");
-
-       //Movimentos do Bispo
-       do{
-        printf("%s cima direita. \n", bispo);
-        b++;
-
-       }while(b <= 5);
+       
+        printf("Movimentos da Torre \n");
+        ftorre(t);
+       
 
        printf("\n");
 
-       //Movimentos da Rainha
-       for(r = 1; r <= 8; r++){
-        printf("%s esquerda. \n", rainha);
-       }
+     
+       printf("Movimentos do Bispo \n");
+        fbispo(b);
+
 
        printf("\n");
 
-       //Movimentos do cavalo
-       for(c = 1; c <= 1; c++){
-          for(int cc = 1; cc <= 2; cc++){
-            printf("%s baixo \n", cavalo);
-          }
-          printf("%s esquerda \n", cavalo);
-       }
 
+        printf("Movimentos da Rainha \n");
+        frainha(r);
+
+
+       printf("\n");
+
+
+     printf("Movimentos do Cavalo \n");
+
+for (int v = 0, h = 0; v < 2; v++, h = (v == 1)) {  //loop externo
+    printf("%s cima\n", cavalo);                    
+
+    for (int step = 0; step < h; step++) {  //loop interno 
+        printf("%s direita\n", cavalo);             
+    }
+}
 
        return 0;
     }
